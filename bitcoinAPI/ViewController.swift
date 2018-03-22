7eb58fda0739edff0670fb127928bc9efeb42680
brn.developers <brn.developers@achiever-33.local>
2018-03-22 10:14:09 +0530

    updated

--- bitcoinAPI/ViewController.swift
+++ bitcoinAPI/ViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        labelText.text = "..."
+        Alamofire.request("https://api.coindesk.com/v1/bpi/currentprice.json").responseJSON { response in
+            
+            print(response)
+            
+            if let bitcoinJSON = response.result.value{
+            let bitcoinObject:Dictionary = bitcoinJSON as! Dictionary<String,Any>
+                let bpiObject:Dictionary = bitcoinObject["bpi"] as! Dictionary<String,Any>
+                let usdObject:Dictionary = bpiObject["USD"] as! Dictionary<String,Any>
+                let rate:Float = usdObject["rate_float"] as! Float
+                self.labelText.text = "$\(rate)"
+            
+                
+            }
+        }
+
+    print("loading web services...")
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+   
 
 }
 
